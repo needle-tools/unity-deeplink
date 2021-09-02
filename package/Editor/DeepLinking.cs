6482ab89571412bfd1eb60379c956ce4953b76d1
Felix Herbst <felix.herbst@gmail.com>
2021-09-02 14:24:20 +0200

    cleanup, remove EditorPatching dependency

--- package/Editor/DeepLinking.cs
+++ package/Editor/DeepLinking.cs
@@ -1,12 +1,13 @@
 ﻿#if UNITY_2019_1_OR_NEWER
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using HarmonyLib;
 using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace needle.EditorPatching
@@ -30,85 +31,111 @@ namespace needle.EditorPatching
         public int Priority { get; set; } = -100000;
     }
     
-    internal class DeepLinking : EditorPatchProvider
+    internal class DeepLinking
     {
+        [InitializeOnLoadMethod]
+        static void InitializePatch()
+        {
+            var type = typeof(UnityEditor.PackageManager.UI.Window).Assembly.GetType("UnityEditor.PackageManager.UI.PackageManagerWindow");
+            var method = type?.GetMethod("OpenURL", (BindingFlags)(-1));
+            if (method == null) return;
+
+            try
+            {
+                var h = new Harmony(nameof(DeepLinking));
+                h.Patch(method, new HarmonyMethod(typeof(DeepLinking), nameof(Prefix)));
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
+        
         // [DeepLink]
+        // // ReSharper disable once UnusedMember.Local
         // private static bool LogDeepLink(string url)
         // {
         //     Debug.Log("[Deep Link] Received: " + url);
         //     return false;
         // }
+        
+        [DeepLink(RegexFilter = @"com.unity3d.kharma:install-package/(.*)")]
+        // ReSharper disable once UnusedMember.Local
+        private static bool InstallPackage(string url)
+        {
+            // UnityEditor.PackageManager.UI.Window.Open(url);
+            InstallPackageByIdentifier(url);
+            return true;
+        }
+
+        private static async void InstallPackageByIdentifier(string identifier)
+        {
+            Debug.Log("[Deep Link] Installing package: " + identifier);
+            var request = Client.Add(identifier);
+            while (!request.IsCompleted)
+                await Task.Delay(100);
+            if (request.Status != StatusCode.Success)
+                Debug.LogWarning("Package " + identifier + " couldn't be installed.");
+        }
 
         // [DeepLink(RegexFilter = @"com.unity3d.kharma:custom\/(.*)")]
+        // // ReSharper disable once UnusedMember.Local
         // private static bool LogCustomData(string data)
         // {
         //     Debug.Log("[Deep Link] Data: " + data);
         //     return true;
         // }
-        
-        public override string DisplayName => "Deep Linking";
-        public override string Description => "Allows to use com.unity3d.kharma: deep links from web sites";
-        protected override void OnGetPatches(List<EditorPatch> patches) => patches.Add(new PackManDeepLinkPatch());
-        public override bool ActiveByDefault => true;
 
-        private class PackManDeepLinkPatch : EditorPatch
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once UnusedParameter.Local
+        private static bool Prefix(object __instance, string url)
         {
-            protected override Task OnGetTargetMethods(List<MethodBase> targetMethods)
-            {
-                var type = typeof(UnityEditor.PackageManager.UI.Window).Assembly.GetType("UnityEditor.PackageManager.UI.PackageManagerWindow");
-                targetMethods.Add(type.GetMethod("OpenURL", (BindingFlags)(-1)));
-                return Task.CompletedTask;
-            }
-
-            private static bool Prefix(object __instance, string url)
-            {
-                // check for regular AssetStore URLs and prevent those from being processed
-                if (Regex.IsMatch(url, @"com.unity3d.kharma:content\/(.*)"))
-                    return true;
-
-                var methods = TypeCache.GetMethodsWithAttribute<DeepLinkAttribute>()
-                    .OrderByDescending(x => x.GetCustomAttribute<DeepLinkAttribute>().Priority)
-                    .ToList();
-                
-                // check if any listener wants to catch this
-                foreach (var method in methods)
-                {
-                    if (method.ReturnType != typeof(bool)) {
-                        Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but does not return bool.");
-                        continue;
-                    }
-
-                    if (!method.IsStatic) {
-                        Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but is not static.");
-                        continue;
-                    }
-
-                    var parameters = method.GetParameters();
-                    if (parameters.Length != 1 || (parameters.Length == 1 && parameters[0].ParameterType != typeof(string))) {
-                        Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but must have exactly one string parameter.");
-                        continue;
-                    }
-
-                    var attr = method.GetCustomAttribute<DeepLinkAttribute>();
-                    if (!string.IsNullOrEmpty(attr.RegexFilter))
-                    {
-                        var match = Regex.Match(url, attr.RegexFilter, attr.RegexOptions);
-                        if(!match.Success)
-                            continue;
-                        if(match.Groups.Count > 1)
-                            url = match.Groups[1].Value;
-                    } 
-
-                    var hasMatched = (bool) method.Invoke(null, new object[] { url });
-                    if (hasMatched)
-                        return false;
-                }
-                
-                // if this is not an AssetStore link, we should not open PackMan for it even if no handler is attached
-                if(methods.Count > 0)
-                    Debug.LogWarning(("[Deep Link] No method found to handle deep link request, will fall back to Unity's handling of it: " + url));
+            // check for regular AssetStore URLs and prevent those from being processed
+            if (Regex.IsMatch(url, @"com.unity3d.kharma:content\/(.*)"))
                 return true;
+
+            var methods = TypeCache.GetMethodsWithAttribute<DeepLinkAttribute>()
+                .OrderByDescending(x => x.GetCustomAttribute<DeepLinkAttribute>().Priority)
+                .ToList();
+            
+            // check if any listener wants to catch this
+            foreach (var method in methods)
+            {
+                if (method.ReturnType != typeof(bool)) {
+                    Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but does not return bool.");
+                    continue;
+                }
+
+                if (!method.IsStatic) {
+                    Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but is not static.");
+                    continue;
+                }
+
+                var parameters = method.GetParameters();
+                if (parameters.Length != 1 || (parameters.Length == 1 && parameters[0].ParameterType != typeof(string))) {
+                    Debug.LogWarning("Method " + method.Name + " is marked as DeepLink but must have exactly one string parameter.");
+                    continue;
+                }
+
+                var attr = method.GetCustomAttribute<DeepLinkAttribute>();
+                if (!string.IsNullOrEmpty(attr.RegexFilter))
+                {
+                    var match = Regex.Match(url, attr.RegexFilter, attr.RegexOptions);
+                    if(!match.Success)
+                        continue;
+                    if(match.Groups.Count > 1)
+                        url = match.Groups[1].Value;
+                } 
+
+                var hasMatched = (bool) method.Invoke(null, new object[] { url });
+                if (hasMatched)
+                    return false;
             }
+            
+            // if this is not an AssetStore link, we should not open PackMan for it even if no handler is attached
+            if(methods.Count > 0)
+                Debug.LogWarning(("[Deep Link] No method found to handle deep link request, will fall back to Unity's handling of it: " + url));
+            return true;
         }
     }
 }
